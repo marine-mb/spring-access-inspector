88a0085955c5bb53c45a5d0a23dbf8e575797f37
Marine du Mesnil <marinemb@theodo.fr>
2025-02-28 11:35:52 +0100

    security: add Secured annotation on Controller

--- sample/microservice-example/src/main/java/com/theodo/ms/SampleApplication.java
+++ sample/microservice-example/src/main/java/com/theodo/ms/SampleApplication.java
@@ -2,8 +2,10 @@ package com.theodo.ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity(securedEnabled = true)
 public class SampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
