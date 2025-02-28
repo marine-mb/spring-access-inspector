205bf8cb285ce511ed6740fd50648b0a8671f3a1
Marine du Mesnil <marinemb@theodo.fr>
2025-02-28 11:36:59 +0100

    security: add PreAuthorize annotation on Controller

--- sample/microservice-example/src/main/java/com/theodo/ms/controller/HousesController.java
+++ sample/microservice-example/src/main/java/com/theodo/ms/controller/HousesController.java
@@ -1,13 +1,15 @@
 package com.theodo.ms.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class HousesController {
 
-    @PostMapping({"/houses/v1/house"})
+    @PostMapping({ "/houses/v1/house" })
+    @PreAuthorize("permitAll()")
     public String postSomething() {
         return "OK";
     }
