343b173d9ed0f1e114946dbe783dbeaaba6b3edf
Marine du Mesnil <marinemb@theodo.fr>
2025-03-05 11:24:46 +0100

    fix(demo project): add isAuthenticated PreAuthorize and user for roles allowed

--- sample/microservice-example/src/main/java/com/theodo/ms/controller/HousesController.java
+++ sample/microservice-example/src/main/java/com/theodo/ms/controller/HousesController.java
@@ -1,8 +1,11 @@
 package com.theodo.ms.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,6 +14,12 @@ public class HousesController {
     @PostMapping({ "/houses/v1/house" })
     @PreAuthorize("permitAll()")
     public String postSomething() {
+        return "OK";
+    }
+
+    @GetMapping({ "/houses/v1/house" })
+    @PreAuthorize("isAuthenticated()")
+    public String getSomething() {
         return "OK";
     }
 
